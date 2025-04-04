c9ba77d4db48c2b23dc0c72db1fa95837438907d
Sergie Code <sergiosnsdk@gmail.com>
2025-04-04 12:27:50 +0100

    Se agregan hijos para mostrar imports

--- src/app/for-sintaxis/for-sintaxis.component.ts
+++ src/app/for-sintaxis/for-sintaxis.component.ts
@@ -1,9 +1,10 @@
-import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { OldForSintaxisComponent } from "./old/old-sintaxis.component";
+import { NewForSintaxisComponent } from "./new/new-sintaxis.component";
 
 @Component({
   selector: 'app-for-sintaxis',
-  imports: [NgFor],
+  imports: [OldForSintaxisComponent, NewForSintaxisComponent, NewForSintaxisComponent, OldForSintaxisComponent],
   templateUrl: './for-sintaxis.component.html',
   styleUrl: './for-sintaxis.component.css'
 })
