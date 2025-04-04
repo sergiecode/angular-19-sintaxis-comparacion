f32b42e6c1c3dfbcd13b87d714c52fca2506e381
Sergie Code <sergiosnsdk@gmail.com>
2025-04-04 12:56:47 +0100

    Se agrega PDF y PPTX con diapositivas explicando

---
+++ src/app/for-sintaxis/new/new-sintaxis.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'new-sintaxis',
+  imports: [],
+  templateUrl: './new-sintaxis.component.html',
+  styleUrl: './new-sintaxis.component.css'
+})
+export class NewForSintaxisComponent {
+  frutas = ['Manzana', 'Banana', 'Naranja', 'Pera'];
+}
---
+++ src/app/for-sintaxis/old/old-sintaxis.component.ts
@@ -0,0 +1,12 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'old-sintaxis',
+  imports: [],
+  templateUrl: './old-sintaxis.component.html',
+  styleUrl: './old-sintaxis.component.css'
+})
+export class OldForSintaxisComponent {
+  frutas = ['Manzana', 'Banana', 'Naranja', 'Pera'];
+}
