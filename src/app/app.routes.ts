8ae8002b545ca5211eb8b20ab13575d8287f1e0e
Sergie Code <sergiosnsdk@gmail.com>
2025-04-11 17:49:28 +0100

    Se agrega componente de pruebas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './home/home.component';
 import { IfSintaxisComponent } from './if-sintaxis/if-sintaxis.component';
 import { SwitchSintaxisComponent } from './switch-sintaxis/switch-sintaxis.component';
 import { ForSintaxisComponent } from './for-sintaxis/for-sintaxis.component';
+import { PruebasComponent } from './pruebas/pruebas.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'if-sintaxis', component: IfSintaxisComponent },
     { path: 'switch-sintaxis', component: SwitchSintaxisComponent },
     { path: 'for-sintaxis', component: ForSintaxisComponent },
+    { path: 'pruebas', component: PruebasComponent },
     { path: '**', redirectTo: '' }
 ];
 
---
+++ src/app/pruebas/pruebas.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pruebas',
+  imports: [],
+  templateUrl: './pruebas.component.html',
+  styleUrl: './pruebas.component.css'
+})
+export class PruebasComponent {
+
+}
